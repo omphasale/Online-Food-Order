07a50834dd36a1a5861fc1f01c2becda944db2fc
Om Phasale <omphasale@gmail.com>
2026-03-17 14:23:11 +0530

    Use dynamic port for server listening

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+// Dynamic port (IMPORTANT)
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.send("Online Food Order App Running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
